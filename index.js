fdcaa069484f416977ce2f6e4663cfe8d71dc376
Elia Gamberi <gamberoatomico@gmail.com>
2020-11-30 22:04:43 +0100

    solved a problem with loading token.txt

--- index.js
+++ index.js
@@ -2,25 +2,55 @@ const Discord = require('discord.js');
 const { getMaxListeners } = require('process');
 const bot = new Discord.Client();
 
-const token = 'Nzc5MDI0NDU5Mzk0MzE4Mzc2.X7ahEA.emc4jBlcXjArcJKAezQDeXWbvPA';
+
+/**
+ * Simple sleep function to solve async problems. It can be solved in a better
+ * way, but i choose this because i dont have what in Italy we call "sbatta"
+ */
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
+
 
 NPepe = 0;
 
+/**
+ * Array where we'll store links for the !pepe command
+ */
 var pepilink = new Array();
 
+/**
+ * With !pepe we'll send a randomized child of this GoogleDrive Folder
+ */
+const idPepeFolder = '1woESiXsG4hEZo56AZf-QdVmdPtv3Xvjp';
+
+/**
+ * Prefix for our commands
+ */
 const PREFIX = '!'
 
+/**
+ * !help for this list to be sent from your bot
+ */
 const help = "Ecco una lista dei comandi:\n\
 "+PREFIX+"help -> fornisce la lista dei comandi\n\
 "+PREFIX+"flip -> lancia una moneta\n\
-"+PREFIX+"roll -> lancia un D30\n\
+"+PREFIX+"roll -> lancia un D30 di default \n\
+"+"e\' possibile aggiungere un argomento per definire il roll massimo \n\
 "+PREFIX+"clear n -> cancella n messaggi dal canale corrente\n\
-"+PREFIX+"pepe -> restituisce un pepe casuale";
+"+PREFIX+"pepe -> restituisce un pepe casuale\n\
+"+PREFIX+"1v1 -> sfidi il bot in un roll casuale da 0 a 100";
 
 
-const fs = require('fs');
+
 const readline = require('readline');
 const {google} = require('googleapis');
+const { waitForDebugger } = require('inspector');
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/drive'];
@@ -30,13 +60,25 @@ const SCOPES = ['https://www.googleapis.com/auth/drive'];
 // time.
 const TOKEN_PATH = 'token.json';
 
-fs.readFile('credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
+const fs = require('fs') 
+
+/**
+ * Secret token, that we read from token.txt, located in our project folder
+ */
+token='';
+
+
+ /**
+  * Sync to FIRST read the file, and THEN trying the token that we 
+  */
+  token = fs.readFileSync('token.txt', 'utf8');
+
+
+  fs.readFile('credentials.json', (err, content) => {
+    if (err) return console.log('Error loading client secret file:', err);
   
-
-  authorize(JSON.parse(content), listPepe);
-});
-
+    authorize(JSON.parse(content), listPepe);
+  });
 
 
 
@@ -50,8 +92,7 @@ function authorize(credentials, callback) {
     if (err) return getAccessToken(oAuth2Client, callback);
     oAuth2Client.setCredentials(JSON.parse(token));
 
-    //callback(oAuth2Client);
-    callback(oAuth2Client,'1woESiXsG4hEZo56AZf-QdVmdPtv3Xvjp')
+    callback(oAuth2Client,idPepeFolder)
   });
 }
 
@@ -98,13 +139,14 @@ function listPepe(auth,fileId)
       if (files.length) {
         console.log('Files:');
         files.map((file) => {
-          console.log('adding '+file.id);
+          //console.log('adding '+file.id);
           pepilink.push('https://drive.google.com/file/d/'+file.id+'/view');
           NPepe++;
         });
       } else {
         console.log('No files found.');
       }
+      console.log('added '+NPepe+' pepe');
     });
 }
 
@@ -134,6 +176,11 @@ function pepe(msg){
   msg.reply('Dal nostro archivio di '+NPepe+' pepe abbiamo trovato questo: '+linkPepe);
 }
 
+function roll1ton(n)
+{
+  return Math.round(Math.random()*(n-1))+1
+}
+
 
 
 bot.on('message', message=>{
@@ -148,11 +195,18 @@ bot.on('message', message=>{
           flip(message)
           break;
       case 'roll':
-          message.reply(Math.round(Math.random()*29)+1)
+        if(!args[1]) 
+          return message.reply('Hai rollato '+roll1ton(30)+' su '+30);
+
+
+        if(!Number.isInteger(parseInt(args[1])))
+          return message.reply('Necessario un numero intero come secondo paramentro :)');
+        
+        return message.reply('Hai rollato '+roll1ton(args[1])+' su '+args[1]);
           break;
       case 'clear':
-          if(!args[1]) return message.reply('Definisci quanti messaggi vuoi cancellare')
-          if(message.author.username != admin) return message.reply('Non sei autorizzato a cancellare messaggi')
+          if(!args[1]) return message.reply('Necessario definire il numero di messaggi da cancellare :)')
+          if(message.author.username != admin) return message.reply('Non sei autorizzato a cancellare messaggi :)')
           message.channel.bulkDelete(args[1]);
           break;
       case 'pepe':
@@ -161,6 +215,27 @@ bot.on('message', message=>{
       case 'fuck':
           message.reply('come ti permetti? 1v1 creativa')
           break;
+      case '1v1':
+          risuser=roll1ton(100);
+          risbot=roll1ton(100);
+          if(risbot>risuser)
+            return message.reply('fai schifo: ho fatto '+risbot+' mentre tu solo '+risuser+': EZ');
+          if(risbot<risuser)
+            return message.reply('ho fatto '+risbot+' e tu '+risuser+': GG');
+          if(risuser==risbot)
+            return message.reply('abbiamo fatto entrambi '+risbot+': REMATCH?');
+          break;
+      case 'buonanotte':
+          message.channel.send('notte notte <3');
+          break;
+      case 'fox':
+          // Call C from JavaScript
+          var result = Module.ccall('c_add', // name of C function
+            'number', // return type
+            ['number', 'number'], // argument types
+            [10, 20]); // arguments
+            // result is 30
+          console.log(result);
   }
 })
 
