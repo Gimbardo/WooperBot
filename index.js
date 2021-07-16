9aa799c3cf630c4c74c58aa93b23199f2e704de8
Elia Gamberi <gamberoatomico@gmail.com>
2021-07-17 01:22:55 +0200

    string to json fixed

--- index.js
+++ index.js
@@ -87,17 +87,17 @@ const discord_credentials = '{"installed": \
   "project_id":"pepi-1605824592804","auth_uri":"https://accounts.google.com/o/oauth2/auth", \
   "token_uri":"https://oauth2.googleapis.com/token", \
   "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs", \
-  "client_secret":'+ process.env.client_secret+', \
+  "client_secret":"'+ process.env.client_secret+'", \
   "redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}'
 
 authorize(JSON.parse(discord_credentials), listFiles);
 
 const drive_token = '{ \
-      "access_token":'+process.env.access_token+', \
-      "refresh_token":'+process.env.refresh_token+', \
-      "scope":'+process.env.scope+', \
-      "token_type":'+process.env.token_type+', \
-      "expiry_date":'+process.env.expiry_date+' \
+      "access_token":"'+process.env.access_token+'", \
+      "refresh_token":"'+process.env.refresh_token+'", \
+      "scope":"'+process.env.scope+'", \
+      "token_type":"'+process.env.token_type+'", \
+      "expiry_date":"'+process.env.expiry_date+'" \
     }';
 
 async function authorize(credentials, callback) {
