cbf8d10a21aa5083413a8765f0906b636835397b
Elia Gamberi <gamberoatomico@gmail.com>
2021-07-17 01:27:12 +0200

    AAAAAAAAAAAAAAAAA

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ const discord_credentials = '{"installed": \
   "project_id":"pepi-1605824592804","auth_uri":"https://accounts.google.com/o/oauth2/auth", \
   "token_uri":"https://oauth2.googleapis.com/token", \
   "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs", \
-  "client_secret":"'+ process.env.client_secret+'", \
+  "client_secret":'+'"'+ process.env.client_secret+'"'+', \
   "redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}'
 
 authorize(JSON.parse(discord_credentials), listFiles);
