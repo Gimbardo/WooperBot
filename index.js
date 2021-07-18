d67286a6177d49e22d1198d2c79e88f53dad2aee
Elia Gamberi <gamberoatomico@gmail.com>
2021-07-18 22:01:00 +0200

    test

--- index.js
+++ index.js
@@ -402,8 +402,8 @@ bot.on('ready', async ()=>{
         if(!args['sound_name'])
           return reply(interaction, 'devi inserire il nome del suono da riprodurre,\nEccoti la lista :monkey::\n'+stringsounds);
         if(sounds.indexOf(args['sound_name']) >= 0){
-          console.log(interaction.user.id)
-          console.log(bot.api.users(user.id).callback.get({
+          console.log(interaction['user']['id'])
+          console.log(bot.api.users(interaction['user']['id']).callback.get({
           }))
           //playFile('.\\sb\\'+args[1]+'.mp3',message);
           return reply(interaction, 'riproducendo il suono, beep boop :robot:')}
