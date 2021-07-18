37d169132228fa9cc375afb760e9001dee81b699
Elia Gamberi <gamberoatomico@gmail.com>
2021-07-18 23:24:20 +0200

    i love console.log

--- index.js
+++ index.js
@@ -404,8 +404,11 @@ bot.on('ready', async ()=>{
         if(sounds.indexOf(args['sound_name']) >= 0){
           console.log(interaction.member.user.id)
           console.log('\n\n\n\n')
-          console.log(bot.api.users(interaction.member.user.id).callback.get({
-          }))
+          console.log(typeof interaction)
+          console.log(typeof bot)
+          console.log(typeof bot.api)
+          // console.log(bot.api.users(interaction.member.user.id).callback.get({
+          // }))
           //playFile('.\\sb\\'+args[1]+'.mp3',message);
           return reply(interaction, 'riproducendo il suono, beep boop :robot:')}
         else
