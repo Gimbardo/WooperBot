6c782af793a06cf83ed6cfeaad4f5d63035c473b
Elia Gamberi <gamberoatomico@gmail.com>
2021-07-20 20:00:46 +0200

    typo fix

--- index.js
+++ index.js
@@ -407,7 +407,7 @@ bot.on('ready', async ()=>{
         if(!args['sound_name'])
           return reply(interaction, 'devi inserire il nome del suono da riprodurre,\nEccoti la lista :monkey::\n'+stringsounds);
         if(sounds.indexOf(args['sound_name']) >= 0){
-          channel = client.channels.cache.get(interaction.channel_id);
+          channel = bot.channels.cache.get(interaction.channel_id);
           console.log('\n\n\n\n')
           console.log(channel)
           channel.send('riproducendo il suono, beep boop :robot:')
